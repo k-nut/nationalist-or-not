f51c6ebb9b8432dd220d6389e0d014c0eb473c75
Knut Hühne <knut@k-nut.eu>
2017-06-23 08:08:22 +0200

    improve formatting

--- app/app.js
+++ app/app.js
@@ -1,13 +1,19 @@
 import getPoliticians from "./api.js"
-import {Stack, Direction} from "swing";
 import { getImageThumbnail } from "./utils.js"
+import { Stack, Direction } from "swing";
 import Toast from "./toast"
 require("./main.css")
 
 
-document.addEventListener('DOMContentLoaded', function () {
+const cardify = (stack) => {
+    Array.from(document.querySelectorAll('.stack li')).forEach(stack.createCard);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    const stack =  Stack();
+
     getPoliticians().then(politicians => {
-        const cardStack = document.querySelector('.stack')
+        const cardStack = document.querySelector('.stack');
         politicians.forEach((pol) => {
             const li = document.createElement("li");
             li.style.backgroundImage = `url(${getImageThumbnail(pol)}`;
@@ -16,30 +22,24 @@ document.addEventListener('DOMContentLoaded', function () {
             li.dataset.party = pol.partyLabel.value;
             cardStack.appendChild(li)
         });
-        cardify()
+        cardify(stack)
     });
-    const stack =  Stack();
 
-
-    const cardify = () => {
-        Array.from(document.querySelectorAll('.stack li')).forEach(stack.createCard);
-    };
-
-    stack.on('throwout', function (e) {
+    stack.on('throwout', function checkThrow(event) {
         // for some reason the same event is sometimes triggered twice. We can tell that this
         // is the second time because we have set the display to none already.
-        if (e.target.style.display === "none"){
+        if (event.target.style.display === "none"){
             return
         }
 
-        const personInfo = e.target.dataset;
+        const personInfo = event.target.dataset;
         const isNationalist = personInfo.nazi === "true";
         const expectedDirection = isNationalist ? Direction.RIGHT : Direction.LEFT;
-        if (e.throwDirection === expectedDirection) {
+        if (event.throwDirection === expectedDirection) {
             Toast.correct(`JA! ${personInfo.name} is a member of ${personInfo.party}`)
         } else {
             Toast.wrong(`NEIN! ${personInfo.name} is a member of ${personInfo.party}`)
         }
-        e.target.style.display = 'none'; // or remove this dom element completely
+        event.target.style.display = 'none'; // or remove this dom element completely
     });
 });
